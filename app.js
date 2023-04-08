39db952423bc0d15f8f91a9ae8c9e0272381ca46
Mark Lai <laimark900317@gmail.com>
2023-04-08 18:56:54 +0800

    test:new script for local and install yargs,dotenv
    * app.js -> log ENV_NAME to see if .env file is loaded successfully
    * .gitignore -> ignore .env file

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
-
 app.use(express.json());
 
+// ===== use .env arguments =====
+const dotenv = require('dotenv')
+const yargs = require('yargs');
+
+const argv = yargs.options({
+        dotenv: {
+            default: '.env.test',
+            describe: 'Path to environment file',
+            type: 'string'
+        }
+    }).argv;
+
+dotenv.config({ path: './.env/' + argv.dotenv });
+console.log("this is arg", process.env.ENV_NAME)
+
 app.get('/', function(req, res) {
-  res.status(200).json({
-    "message":"this is the test of CD"
-  });
+    res.status(200).json({
+        "message":"this is the test of CD"
+    });
 });
 
 module.exports = app;
