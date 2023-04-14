f63d3c32fb7e22133b9ca01925f5125ebaca6fb7
Mark Lai <laimark900317@gmail.com>
2023-04-15 03:21:44 +0800

    feature: add swagger
    * package.json -> install swagger
    * hello.js -> swagger comment
    * app.js -> old app.js deleted
    * hello.test.js -> test case add

--- src/routes/hello.js
+++ src/routes/hello.js
@@ -1,13 +1,41 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = function (dependencies) {
-    
-    router.get('/', function(req, res) {
-        res.status(200).json({
-            "message":"this is the test of CD"
-        });
-    });
 
-    return router
-}
+    
+
+/**
+ * Say hello to a user.
+ * @swagger
+ * /hello:
+ *   get:
+ *     summary: Say hello to a user
+ *     description: This API returns a greeting message to the specified user.
+ *     parameters:
+ *       - name: user
+ *         in: query
+ *         description: The name of the user to greet.
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: A greeting message for the specified user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: The greeting message.
+ *     tags:
+ *       - Greetings
+ */
+router.get('/', function(req, res) {
+    const user = req.query.user
+    res.status(200).json({
+        "message":`this is the test of ${user}`
+    });
+});
+
+module.exports = router
