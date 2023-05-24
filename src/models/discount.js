17a1b1c9a82affca43bf9d0427cfcdad652cdae4
Wiki4831 <hyes96361@gmail.com>
2023-05-24 20:31:06 +0800

    fix model/discount,order,staff

--- src/models/discount.js
+++ src/models/discount.js
@@ -3,22 +3,34 @@ const { poolQuery } = require("../utils/mariadb");
 const getAll = async (userId) => {
   const query = `SELECT discount_id, discount_name, discount_value, discount_note FROM discount WHERE user_id = ?`;
   const params = [userId];
-  const result = await poolQuery(query, params);
-  return result;
+  try {
+    const result = await poolQuery(query, params);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const create = async (discount) => {
   const query = `INSERT INTO discount SET ?`;
   const params = [discount];
-  const result = await poolQuery(query, params);
-  return result;
+  try {
+    const result = await poolQuery(query, params);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const remove = async (userId, discountId) => {
   const query = `DELETE FROM discount WHERE user_id = ? AND discount_id = ?`;
   const params = [userId, discountId];
-  const result = await poolQuery(query, params);
-  return result;
+  try {
+    const result = await poolQuery(query, params);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const isExist = async (userId, discountId, discountName) => {
@@ -31,8 +43,12 @@ const isExist = async (userId, discountId, discountName) => {
     query = `SELECT COUNT(*) AS count FROM discount WHERE user_id = ? AND discount_name = ?`;
     params = [userId, discountName];
   }
-  const result = await poolQuery(query, params);
-  return result[0].count > 0;
+  try {
+    const result = await poolQuery(query, params);
+    return result[0].count > 0;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = { getAll, create, remove, isExist };
--- src/models/staff.js
+++ src/models/staff.js
@@ -3,8 +3,12 @@ const { poolQuery } = require("../utils/mariadb");
 const getAll = async (userId) => {
   const query = `SELECT staff_id, staff_name, staff_email, staff_phone_number FROM staff WHERE user_id = ?`;
   const params = [userId];
-  const result = await poolQuery(query, params);
-  return result;
+  try {
+    const result = await poolQuery(query, params);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const getById = async (userId, staffId) => {
@@ -12,29 +16,45 @@ const getById = async (userId, staffId) => {
     "SELECT staff_id, staff_name, staff_email, staff_phone_number \
     FROM staff WHERE user_id = ? AND staff_id = ?";
   const params = [userId, staffId];
-  const result = await poolQuery(query, params);
-  return result;
+  try {
+    const result = await poolQuery(query, params);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const create = async (staff) => {
   const query = "INSERT INTO staff SET ?";
   const params = [staff];
-  const result = await poolQuery(query, params);
-  return result;
+  try {
+    const result = await poolQuery(query, params);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const update = async (staff) => {
   const query = "UPDATE staff SET ? WHERE staff_id = ?";
   const params = [staff, staff.staff_id];
-  const result = await poolQuery(query, params);
-  return result;
+  try {
+    const result = await poolQuery(query, params);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const remove = async (userId, staffId) => {
   const query = "DELETE FROM staff WHERE user_id = ? AND staff_id = ?";
   const params = [userId, staffId];
-  const result = await poolQuery(query, params);
-  return result;
+  try {
+    const result = await poolQuery(query, params);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const isExist = async (userId, staffId, staffName) => {
@@ -49,23 +69,35 @@ const isExist = async (userId, staffId, staffName) => {
       "SELECT COUNT(*) AS count FROM staff WHERE user_id = ? AND staff_name = ?";
     params = [userId, staffName];
   }
-  const result = await poolQuery(query, params);
-  return result[0].count > 0;
+  try {
+    const result = await poolQuery(query, params);
+    return result[0].count > 0;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const getStaffIdByName = async (userId, staffName) => {
   const query = "SELECT staff_id FROM staff WHERE user_id = ? AND staff_name = ?";
   const params = [userId, staffName];
-  const result = await poolQuery(query, params);
-  return result[0].staff_id;
+  try{
+    const result = await poolQuery(query, params);
+    return result[0].staff_id;
+  }catch(err){
+    console.log(err);
+  }
 };
 
 const isExistName = async (userId, staffId, staffName) => {
   const query =
     "SELECT COUNT(*) AS count FROM staff WHERE user_id = ? AND staff_id != ? AND staff_name = ?";
   const params = [userId, staffId, staffName];
-  const result = await poolQuery(query, params);
-  return result[0].count > 0;
+  try {
+    const result = await poolQuery(query, params);
+    return result[0].count > 0;
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = {
